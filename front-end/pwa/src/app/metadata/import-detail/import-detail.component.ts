3f280c6a5b7bb0ae82d191fe755d9dba90b5a784
patowhiz <conlooptechnologies@gmail.com>
2024-06-04 12:56:44 +0300

    cleaned up import component

--- front-end/pwa/src/app/metadata/import-detail/import-detail.component.ts
+++ front-end/pwa/src/app/metadata/import-detail/import-detail.component.ts
@@ -7,25 +7,6 @@ import { Component } from '@angular/core';
 })
 export class ImportDetailComponent {
 
-  //TODO. Left here
-
-  protected stationColPosition: number | null = null;
-  protected elementColPositions: number[] = [];
-  protected elementIds: number[] = [];
-
-
-  protected enforceLimitCheck!: boolean;
-  protected formDescription!: number | null;
-  protected utcTimeDifference!: string | null;
-
-  protected onStationColumnChange(stationColumnPosition: number | null): void {
-    this.stationColPosition = stationColumnPosition;
-  }
-
-  protected onElementColumnSelection(option : 'SINGLE' | 'MULTIPLE'):void{
-
-
-
-  }
+ 
 
 }
