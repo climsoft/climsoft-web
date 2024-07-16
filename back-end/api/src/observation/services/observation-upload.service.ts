2c4854c94593dfa43cc6980bfb67f1ecf712bed3
patowhiz <conlooptechnologies@gmail.com>
2024-07-16 07:51:25 +0300

    changed duckdb dependency. Added script transformation.

--- back-end/api/src/observation/services/observation-upload.service.ts
+++ back-end/api/src/observation/services/observation-upload.service.ts
@@ -1,20 +1,14 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 
-import { CreateObservationDto } from '../dtos/create-observation.dto';
-import { Index } from 'typeorm';
-import { ObservationsService } from './observations.service';
-import { isNumber } from 'class-validator';
-import { StringUtils } from 'src/shared/utils/string.utils';
-import { QCStatusEnum } from '../enums/qc-status.enum';
-import { FlagEnum } from '../enums/flag.enum';
+import { CreateObservationDto } from '../dtos/create-observation.dto'; 
+import { ObservationsService } from './observations.service'; 
 import { CreateImportTabularSourceDTO } from 'src/metadata/controllers/sources/dtos/create-import-source-tabular.dto';
 
 import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
-import * as duckdb from 'duckdb';
-import { SourcesService } from 'src/metadata/controllers/sources/services/sources.service';
-import { ViewSourceDto } from 'src/metadata/controllers/sources/dtos/view-source.dto';
+import { Database } from "duckdb-async";
+import { SourcesService } from 'src/metadata/controllers/sources/services/sources.service'; 
 import { CreateImportSourceDTO } from 'src/metadata/controllers/sources/dtos/create-import-source.dto';
 
 
@@ -25,24 +19,16 @@ interface UploadedObservationDto extends CreateObservationDto {
 @Injectable()
 export class ObservationUploadService {
 
-    private db: duckdb.Database;
+    private db: Database;
     private tempFilesFolderPath: string;
 
     constructor(
         private observationsService: ObservationsService,
         private sourcesService: SourcesService) {
 
-        this.setupFolder()
+        this.setupDuckDB();
+        this.setupFolder();
 
-        this.db = new duckdb.Database(':memory:');
-
-        // this.db.all("SELECT * FROM duckdb_settings() WHERE name = 'threads';", function (err, res) {
-        //     if (err) {
-        //         console.warn(err);
-        //         return;
-        //     }
-        //     console.log('duckdb res: ', res)
-        // });
     }
 
     async setupFolder(): Promise<void> {
@@ -59,17 +45,20 @@ export class ObservationUploadService {
             } catch (err2) {
                 console.error("Could not create temporary folder: ", err2);
                 // TODO. Throw appropriiate error.
+                throw err2;
             }
 
         }
 
     }
 
+    async setupDuckDB() {
+        this.db = await Database.create(":memory:");
+    }
 
     async processFile(sourceId: number, file: Express.Multer.File, userId: number) {
 
         const newFileName: string = `${this.tempFilesFolderPath}/user_${userId}_observations_upload${path.extname(file.originalname)}`;
-
 
         // Save the file to the temporary directory
         try {
@@ -77,169 +66,202 @@ export class ObservationUploadService {
         } catch (err) {
             console.error('Could not save user file', err);
             // TODO. Through an error.
+            throw new Error("Could not save user file");
         }
 
-        // Get the source using the source id
+        // Get the source definition using the source id
         const sourceDefinition: CreateImportSourceDTO = (await this.sourcesService.find(sourceId)).extraMetadata as CreateImportSourceDTO;
+
         if (sourceDefinition.format === "TABULAR") {
             this.importTabularSource(sourceDefinition as CreateImportTabularSourceDTO, newFileName);
         } else {
-            return JSON.stringify('error: source not supported yet.');
+            console.error('Source not supported yet');
+            // TODO. Throw correct error.
+            throw new Error("Source not supported yet");
         }
-
-        console.log("Source Id", sourceId);
-        //TODO. Left here.
-
-        //Get source
-        //Create the table.
-        //TODO. Later do the actual import status through server sent events in the front end to check progress of duckdb import operation.
-
-
-
-
 
         return JSON.stringify('success');
     }
 
 
+    private async importTabularSource(source: CreateImportTabularSourceDTO, fileName: string) {
 
-    //-----------------------
+        try {
+            const tableName: string = path.basename(fileName, path.extname(fileName));
+            const importParams: string[] = ['all_varchar = true', 'header = false', `skip = ${source.rowsToSkip}`];
 
-    private importTabularSource(source: CreateImportTabularSourceDTO, fileName: string): void {
-        const columns: string[] = [];
+            if (source.delimiter) {
+                importParams.push(`delim = '${source.delimiter}'`);
+            }
+
+            await this.db.run(`CREATE TABLE ${tableName} AS SELECT * FROM read_csv('${fileName}',  ${importParams.join(",")})`);
 
 
-        const tableName: string = path.basename(fileName, path.extname(fileName));
-        const params: string[] = ['all_varchar = true', 'header = false', `skip = ${source.rowsToSkip}`];
+            // Process columns data
+            let sql: string;
+            sql = this.alterStationColumn(source, tableName);
+            sql = sql + this.alterElementAndValueColumn(source, tableName);
+            sql = sql + this.alterPeriodColumn(source, tableName);
+            sql = sql + this.alterElevationColumn(source, tableName);
+            sql = sql + this.alterDateColumn(source, tableName);
 
-        if (source.delimiter) {
-            params.push(`delim = '${source.delimiter}'`);
+            console.log('SQL', sql);
+
+            await this.db.exec(sql);
+
+            const colNames = await this.db.all(`DESCRIBE SELECT * FROM ${tableName}`);
+
+            console.log('Column names', colNames)
+
+            //this.db.all(`SELECT * FROM ${tableName}`);
+        } catch (error) {
+            console.error("Import error: ", error);
+            // TODO, Throw correct error 
+            throw error;
         }
 
-        const sql: string = `CREATE TABLE ${tableName} AS SELECT * FROM read_csv('${fileName}',  ${params.join(",")})`;
-
-        console.log('sql: ', sql);
-
-        // if (1 === 1) {
-        //     return;
-        // }
-
-        this.db.run(sql,  (err) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                this.GetFileColumns(tableName);
-
-
-            });
     }
 
-    private GetFileColumns(tableName: string) {
-        this.db.all(`DESCRIBE SELECT * FROM ${tableName}`,  (err, res)=> {
-            if (err) {
-                console.warn(err);
-                return;
-            }
-            console.log('describe response: ', res);
+    private alterStationColumn(source: CreateImportTabularSourceDTO, tableName: string, defaultStationId?: string): string {
 
-            // TODO. Left here
-        });
-    }
+        let sql: string;
 
-    private createColumns(sourceDefinition: CreateImportTabularSourceDTO): string[] {
-        const columns: string[] = this.createColumns(sourceDefinition);
-
-        this.addStationColumn(sourceDefinition, columns);
-        this.addElementAndValueColumn(sourceDefinition, columns);
-        this.addPeriodColumn(sourceDefinition, columns);
-        this.addElevationColumn(sourceDefinition, columns);
-        this.addDateColumn(sourceDefinition, columns);
-
-        return columns;
-    }
-
-
-    private addStationColumn(source: CreateImportTabularSourceDTO, columns: string[]) {
         if (source.stationDefinition) {
-            columns.splice(source.stationDefinition.columnPosition - 1, 0, "station_id VARCHAR");
-        }
-    }
+            sql = `ALTER TABLE ${tableName} RENAME column${source.stationDefinition.columnPosition - 1} TO station_id;`;
+            sql = sql + `ALTER TABLE ${tableName} ALTER COLUMN station_id SET NOT NULL;`;
 
-    private addElementAndValueColumn(source: CreateImportTabularSourceDTO, columns: string[]) {
-        if (source.elementAndValueDefinition) {
-
-            if (source.elementAndValueDefinition.noElement) {
-
-                columns.splice(source.elementAndValueDefinition.noElement.valueColumnPosition - 1, 0, "value VARCHAR");
-
-                if (source.elementAndValueDefinition.noElement.flagColumnPosition) {
-                    columns.splice(source.elementAndValueDefinition.noElement.flagColumnPosition - 1, 0, "flag VARCHAR");
-                }
-
-
-            } else if (source.elementAndValueDefinition.hasElement) {
-
-                if (source.elementAndValueDefinition.hasElement.singleColumn) {
-
-                    columns.splice(source.elementAndValueDefinition.hasElement.singleColumn.elementColumnPosition - 1, 0, "element VARCHAR");
-
-                    columns.splice(source.elementAndValueDefinition.hasElement.singleColumn.valueColumnPosition - 1, 0, "value VARCHAR");
-
-                    if (source.elementAndValueDefinition.hasElement.singleColumn.flagColumnPosition) {
-                        columns.splice(source.elementAndValueDefinition.hasElement.singleColumn.flagColumnPosition - 1, 0, "flag VARCHAR");
-                    }
-
-                } else if (source.elementAndValueDefinition.hasElement.multipleColumn) {
-                    for (const el of source.elementAndValueDefinition.hasElement.multipleColumn) {
-                        columns.splice(el.columnPosition - 1, 0, el.databaseId + " VARCHAR");
-                    }
-                }
-
-
+            if (source.stationDefinition.stationsToFetch) {
+                // TODO. SQL for getting the stations to fetch only.
             }
+
+        } else if (defaultStationId) {
+            sql = `ALTER TABLE ${tableName} ADD COLUMN station_id VARCHAR DEFAULT ${defaultStationId};`;
+        } else {
+            throw new Error("Station must be provided");
         }
+
+        return sql;
+
     }
 
-    private addPeriodColumn(source: CreateImportTabularSourceDTO, columns: string[]) {
+
+    private alterElementAndValueColumn(source: CreateImportTabularSourceDTO, tableName: string): string {
+        let sql: string = "";
+
+        if (source.elementAndValueDefinition.noElement) {
+
+            sql = `ALTER TABLE ${tableName} RENAME column${source.elementAndValueDefinition.noElement.valueColumnPosition - 1} TO value;`;
+
+            if (source.elementAndValueDefinition.noElement.flagColumnPosition) {
+                sql = sql + `ALTER TABLE ${tableName} RENAME column${source.elementAndValueDefinition.noElement.flagColumnPosition - 1} TO flag;`;
+            } else {
+                sql = sql + `ALTER TABLE ${tableName} ADD COLUMN flag VARCHAR DEFAULT NULL;`;
+            }
+
+
+        } else if (source.elementAndValueDefinition.hasElement) {
+
+            if (source.elementAndValueDefinition.hasElement.singleColumn) {
+
+                //--------------------------
+                // Element column
+                sql = `ALTER TABLE ${tableName} RENAME column${source.elementAndValueDefinition.hasElement.singleColumn.elementColumnPosition - 1} TO element_id;`;
+                if (source.elementAndValueDefinition.hasElement.singleColumn.elementsToFetch) {
+                    // TODO. SQL for getting the elements to fetch
+                }
+                sql = sql + `ALTER TABLE ${tableName} ALTER COLUMN element_id SET NOT NULL;`;
+                sql = sql + `ALTER TABLE ${tableName} ALTER element_id TYPE REAL;`;
+                //--------------------------
+
+                //--------------------------
+                // Value column
+                sql = sql + `ALTER TABLE ${tableName} RENAME column${source.elementAndValueDefinition.hasElement.singleColumn.valueColumnPosition - 1} TO value;`;
+                sql = sql + `ALTER TABLE ${tableName} ALTER COLUMN value SET DEFAULT NULL;`;
+                sql = sql + `ALTER TABLE ${tableName} ALTER value TYPE REAL;`;
+                //--------------------------
+
+                //--------------------------
+                // Flag column
+                if (source.elementAndValueDefinition.hasElement.singleColumn.flagColumnPosition) {
+                    sql = sql + `ALTER TABLE ${tableName} RENAME column${source.elementAndValueDefinition.hasElement.singleColumn.flagColumnPosition - 1} TO flag;`
+                } else {
+                    sql = sql + `ALTER TABLE ${tableName} ADD COLUMN flag VARCHAR DEFAULT NULL;`;
+                }
+                //--------------------------
+
+
+
+            } else if (source.elementAndValueDefinition.hasElement.multipleColumn) {
+                // TODO. Write sql for stacking the multiple element column values
+                for (const el of source.elementAndValueDefinition.hasElement.multipleColumn) {
+                    //columns.splice(el.columnPosition - 1, 0, el.databaseId + "");
+                }
+            }
+
+
+        }
+
+        return sql;
+
+    }
+
+    private alterPeriodColumn(source: CreateImportTabularSourceDTO, tableName: string): string {
+
+        let sql: string = "";
 
         if (source.periodDefinition.columnPosition) {
-            columns.splice(source.periodDefinition.columnPosition - 1, 0, "period VARCHAR");
+            sql = `ALTER TABLE ${tableName} RENAME column${source.periodDefinition.columnPosition - 1} TO period;`
+            sql = sql + `ALTER TABLE ${tableName} ALTER COLUMN period SET NOT NULL;`;
+            sql = sql + `ALTER TABLE ${tableName} ALTER period TYPE INTEGER;`;
+        } else {
+            sql = `ALTER TABLE ${tableName} ADD COLUMN period INTEGER DEFAULT ${source.periodDefinition.defaultPeriod};`;
         }
+
+        return sql;
 
     }
 
-    private addElevationColumn(source: CreateImportTabularSourceDTO, columns: string[]) {
+    private  alterElevationColumn(source: CreateImportTabularSourceDTO, tableName: string): string {
+        let sql: string = "";
         if (source.elevationColumnPosition) {
-            columns.splice(source.elevationColumnPosition - 1, 0, "elevation VARCHAR");
+            sql = `ALTER TABLE ${tableName} RENAME column${source.elevationColumnPosition - 1} TO elevation;`;
+            sql = sql + `ALTER TABLE ${tableName} ALTER COLUMN elevation SET NOT NULL;`;
+            sql = sql + `ALTER TABLE ${tableName} ALTER elevation REAL INTEGER;`;
+        } else {
+            sql = `ALTER TABLE ${tableName} ADD COLUMN elevation REAL DEFAULT 0;`;
         }
+
+        return sql;
     }
 
-    private addDateColumn(source: CreateImportTabularSourceDTO, columns: string[]) {
+    private alterDateColumn(source: CreateImportTabularSourceDTO, tableName: string): string {
 
+        let sql: string = "";
         if (source.datetimeDefinition.dateTimeColumnPostion !== undefined) {
-            columns.splice(source.datetimeDefinition.dateTimeColumnPostion - 1, 0, "date_time VARCHAR");
+            sql = `ALTER TABLE ${tableName} RENAME column${source.datetimeDefinition.dateTimeColumnPostion - 1} TO date_time;`;
+            sql = sql + `ALTER TABLE ${tableName} ALTER COLUMN date_time SET NOT NULL;`;
 
+            //TODO. Left here. Convert the datetime.
 
         } else if (source.datetimeDefinition.dateTimeInMultipleColumn) {
 
             if (source.datetimeDefinition.dateTimeInMultipleColumn.dateInSingleColumn) {
-                columns.splice(source.datetimeDefinition.dateTimeInMultipleColumn.dateInSingleColumn.dateColumnPosition - 1, 0, "date VARCHAR");
+                // columns.splice(source.datetimeDefinition.dateTimeInMultipleColumn.dateInSingleColumn.dateColumnPosition - 1, 0, "date");
 
             } else if (source.datetimeDefinition.dateTimeInMultipleColumn.dateInMultipleColumn) {
-                columns.splice(source.datetimeDefinition.dateTimeInMultipleColumn.dateInMultipleColumn.yearColumnPosition - 1, 0, "year VARCHAR");
-                columns.splice(source.datetimeDefinition.dateTimeInMultipleColumn.dateInMultipleColumn.monthColumnPosition - 1, 0, "month VARCHAR");
-                columns.splice(source.datetimeDefinition.dateTimeInMultipleColumn.dateInMultipleColumn.dayColumnPosition - 1, 0, "day VARCHAR");
+                // columns.splice(source.datetimeDefinition.dateTimeInMultipleColumn.dateInMultipleColumn.yearColumnPosition - 1, 0, "year");
+                //  columns.splice(source.datetimeDefinition.dateTimeInMultipleColumn.dateInMultipleColumn.monthColumnPosition - 1, 0, "month");
+                //  columns.splice(source.datetimeDefinition.dateTimeInMultipleColumn.dateInMultipleColumn.dayColumnPosition - 1, 0, "day");
 
             }
 
             if (source.datetimeDefinition.dateTimeInMultipleColumn.hourDefinition.columnPosition) {
-                columns.splice(source.datetimeDefinition.dateTimeInMultipleColumn.hourDefinition.columnPosition - 1, 0, "hour VARCHAR");
+                //columns.splice(source.datetimeDefinition.dateTimeInMultipleColumn.hourDefinition.columnPosition - 1, 0, "hour");
             }
 
         }
 
-
+        return sql;
     }
 
 
