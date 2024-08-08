d691a2eb78b1b700a147760a4768ff549325b2da
patowhiz <conlooptechnologies@gmail.com>
2024-08-08 16:05:09 +0300

    added region and drainage metadata and  changed station metadata

---
+++ back-end/api/src/metadata/entities/Regions/admin-region.entity.ts
@@ -0,0 +1,22 @@
+import { BaseEntity, BaseLogVo } from "src/shared/entity/base-entity";
+import { Column, Entity, PrimaryColumn } from "typeorm"; 
+
+@Entity("admin_regions")
+export class AdminRegionsEntity extends BaseEntity {
+  @PrimaryColumn({ type: 'int' })
+  id: number;
+
+  @Column({ type: 'varchar', unique: true })
+  name: string;
+
+  @Column({ type: 'varchar' })
+  description: string;
+
+  // TODO. Later code a transformer to tansform this
+  @Column({ type: 'polygon'})  
+  location: string; //@Index({ spatial: true }) // TODO, index this after the move to POSTGIS
+
+  @Column({ type: 'jsonb', nullable: true })
+  log: BaseLogVo[] | null;
+}
+
