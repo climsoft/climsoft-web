7369643752484f4e7d78f22459638c24f07ddb30
patowhiz <conlooptechnologies@gmail.com>
2024-09-05 15:38:50 +0300

    Added initial sql scripts for qc test and observation log

---
+++ back-end/api/src/sql-scripts/observation_log.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE FUNCTION func_update_observations_log_column()
+RETURNS TRIGGER AS $$
+BEGIN
+        IF (
+            NEW.value IS DISTINCT FROM OLD.value OR 
+            NEW.flag IS DISTINCT FROM OLD.flag OR 
+            NEW.final IS DISTINCT FROM OLD.final OR 
+            NEW.comment IS DISTINCT FROM OLD.comment OR 
+            NEW.deleted IS DISTINCT FROM OLD.deleted
+        ) THEN
+            NEW.log := COALESCE(OLD.log, '[]'::JSONB) || jsonb_build_object(
+            'value', OLD.value,
+            'flag', OLD.flag,
+            'final', OLD.final,
+            'comment', OLD.comment,
+            'entryUserId', OLD.entry_user_id,
+            'deleted', OLD.deleted,
+            'entryDateTime', CURRENT_TIMESTAMP
+        );
+        END IF;
+ RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER trg_update_observations_log_column
+BEFORE UPDATE ON observations
+FOR EACH row
+EXECUTE FUNCTION func_update_observations_log_column();
