f4661449bb1ca5ddcad27a7128224f99e8925cbe
patowhiz <conlooptechnologies@gmail.com>
2024-11-22 17:18:13 +0300

    removed console log

--- front-end/pwa/src/app/metadata/stations/stations-search-dialog/stations-search-dialog.component.ts
+++ front-end/pwa/src/app/metadata/stations/stations-search-dialog/stations-search-dialog.component.ts
@@ -28,7 +28,6 @@ export class StationsSearchDialogComponent {
   protected saveSearch: boolean = false;
 
   constructor(private stationsCacheService: StationsCacheService) { 
-    console.log('dialog initialised')
   }
 
   public openDialog(): void {
