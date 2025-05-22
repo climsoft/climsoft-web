9eecd84fdba2b282b05a3de3f788a0b181439166
patowhiz <conlooptechnologies@gmail.com>
2025-05-22 11:53:37 +0300

    more improvements

--- front-end/pwa/src/app/metadata/stations/view-stations/view-stations-table/view-stations-table.component.ts
+++ front-end/pwa/src/app/metadata/stations/view-stations/view-stations-table/view-stations-table.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { take } from 'rxjs';
+import { Subject, take, takeUntil } from 'rxjs';
 import { AppAuthService } from 'src/app/app-auth.service';
 import { StationCacheModel } from 'src/app/metadata/stations/services/stations-cache.service';
 
@@ -9,11 +9,13 @@ import { StationCacheModel } from 'src/app/metadata/stations/services/stations-c
   templateUrl: './view-stations-table.component.html',
   styleUrls: ['./view-stations-table.component.scss']
 })
-export class ViewStationsTableComponent implements OnChanges {
+export class ViewStationsTableComponent implements OnChanges, OnDestroy {
   @Input()
   public stations!: StationCacheModel[];
 
   protected showEditButton: boolean = false;
+
+  private destroy$ = new Subject<void>();
 
   constructor(
     private router: Router,
@@ -21,22 +23,27 @@ export class ViewStationsTableComponent implements OnChanges {
     private appAuthService: AppAuthService,) { }
 
   ngOnChanges(changes: SimpleChanges): void {
-     if (this.stations) {
-          // Check on allowed options
-          this.appAuthService.user.pipe(
-            take(1),
-          ).subscribe(user => {
-            if (!user) {
-              throw new Error('User not logged in');
-            }
-    
-            // Only show edit button if user is admin
-            // However, other users allowed to edit a station can always click on it.
-            if (user.isSystemAdmin) {
-              this.showEditButton = true;
-            }
-          });
+    if (this.stations) {
+      // Check on allowed options
+      this.appAuthService.user.pipe(
+        takeUntil(this.destroy$),
+      ).subscribe(user => {
+        if (!user) {
+          throw new Error('User not logged in');
         }
+
+        // Only show edit button if user is admin
+        // However, other users allowed to edit a station can always click on it.
+        if (user.isSystemAdmin) {
+          this.showEditButton = true;
+        }
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   protected onStationClicked(station: StationCacheModel) {
